298a9c4ca868fbb61a9c3eb747866c479eef7f35
Gsamini <ahmad.amini580@gmail.com>
2022-05-12 14:55:06 -1200

    PROJECT

---
+++ GitHubClass.java
@@ -0,0 +1,14 @@
+package GitHubClassProject;
+
+public class GitHubClass {
+	public static void main(String[] args) {
+		
+	
+ int a = 10;
+ int b = 10;
+ int c = a-b;
+ String d = "Today";
+ 
+ System.out.println(c);
+}
+}
